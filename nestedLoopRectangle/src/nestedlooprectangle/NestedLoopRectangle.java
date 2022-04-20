8afc6b9fe242be3153bb54aa2da9da010e18a292
Kevin Bell <bellKevin@pm.me>
2022-04-20 02:37:37 +0000

    Add files via upload

---
+++ nestedLoopRectangle/src/nestedlooprectangle/NestedLoopRectangle.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nestedlooprectangle;
+
+/**
+ * NestedLoopRectangle.java Dean & Dean
+ *
+ * This program uses nest looping to draw a rectangle
+ *
+ * @author 4800590195
+ */
+import java.util.Scanner;
+
+public class NestedLoopRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        int height, width; //rectangle's dimensions
+        char printCharacter;
+
+        System.out.print("Enter height: ");
+        height = computerKeyboardInput.nextInt();
+        System.out.print("Enter width: ");
+        width = computerKeyboardInput.nextInt();
+        System.out.print("Enter character: ");
+        printCharacter = computerKeyboardInput.next().charAt(0);
+
+        for (int row = 1; row <= height; row++) {
+            for (int col = 1; col <= width; col++) {
+                System.out.print(printCharacter);
+            }
+            System.out.println();
+        }
+    } //end main
+} // end class NestedLoopRectangle
